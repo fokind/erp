b2192e99eb9eaaff7f0b20fbeb284a651ed2a3ae
Dmitry Fokin <fokind1@yandex.ru>
2018-04-06 09:07:47 +0300

    рефакторинг

--- client/controller/Employee.controller.js
+++ client/controller/Employee.controller.js
@@ -2,16 +2,16 @@
 'use strict';
 
 sap.ui.define([
-  'tms/basic/controller/BaseController',
+  'tms/basic/controller/InstanceController',
 ], function(Controller) {
   return Controller.extend('tms.basic.controller.Employee', {
     onInit: function() {
       let that = this;
       let oRouter = sap.ui.core.UIComponent.getRouterFor(that);
       oRouter.getRoute('employee').attachPatternMatched(that._onRouteMatched, that);
-      
-      this.modelName = 'Employees';
-      this.models = [
+
+      that.modelName = 'Employees';
+      that.models = [
         'Departments',
       ];
 
